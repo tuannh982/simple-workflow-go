ea142d6c4612a7c4246fd3eba0063696f3fcee5e
tuannh982 <tuannh982@gmail.com>
2024-11-12 11:03:56 +0700

    reduce race condition

--- test/e2e/psql/init.go
+++ test/e2e/psql/init.go
@@ -8,6 +8,9 @@ import (
 	"github.com/tuannh982/simple-workflows-go/pkg/backend/psql"
 	"github.com/tuannh982/simple-workflows-go/pkg/dataconverter"
 	"go.uber.org/zap"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
 	"os"
 	"time"
 )
@@ -20,12 +23,27 @@ const (
 	DbPassword = "123456"
 )
 
+var (
+	gormConfig = &gorm.Config{
+		Logger: logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+			logger.Config{
+				SlowThreshold:             time.Second,   // Slow SQL threshold
+				LogLevel:                  logger.Silent, // Log level
+				IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
+				ParameterizedQueries:      true,          // Don't include params in the SQL log
+				Colorful:                  false,         // Disable color
+			},
+		),
+	}
+)
+
 func InitBackend(logger *zap.Logger) (backend.Backend, error) {
 	hostname, err := os.Hostname()
 	if err != nil {
 		return nil, err
 	}
-	db, err := psql.Connect(DbHost, DbPort, DbUser, DbPassword, DbName, nil)
+	db, err := psql.Connect(DbHost, DbPort, DbUser, DbPassword, DbName, gormConfig)
 	if err != nil {
 		return nil, err
 	}
